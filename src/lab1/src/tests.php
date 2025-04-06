1aa77fc7f637763218c3eafe9c2c9d1d98af2be9
Anatoliy Denezhniy <forest-chan@mail.ru>
2025-04-06 16:10:53 +0300

    des algorithm realisation

---
+++ src/lab1/src/tests.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+require_once 'des.php';
+
+$testCases = [
+    [
+        'plaintText' => 'hello',
+        'key' => '133457799BBCDFF1',
+    ],
+    [
+        'plaintText' => 'world',
+        'key' => '0F1571C947D9E859',
+    ],
+];
+
+echo PHP_EOL;
+foreach ($testCases as $testIndex => $testCase) {
+    echo 'Running test #' . $testIndex + 1 . PHP_EOL;
+
+    main(
+        plainText: $testCase['plaintText'],
+        key: $testCase['key'],
+    );
+
+    echo 'End test' . PHP_EOL . PHP_EOL;
+}
+
+// атака встреча посередине на 2des
